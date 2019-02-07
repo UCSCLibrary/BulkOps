a7c8c33e1894f64fce195c25d35edd0f8ed58139
NedHenry <ethenry@ucsc.edu>
2019-02-07 11:50:55 -0800

    add job to do spreadsheet verification in the background

---
+++ lib/bulk_ops/verification_job.rb
@@ -0,0 +1,21 @@
+#require 'hydra/access_controls'
+#require 'hyrax/workflow/activate_object'
+
+class BulkOps::VerificationJob < ActiveJob::Base
+
+  attr_accessor :operation
+
+  queue_as :default
+
+  def perform(operation)
+
+      if @operation.verify!
+        @operation.set_stage "authorize"
+        if @operation.create_pull_request
+          @operation.notify(subject: "Bulk Operation Verification Successful", message: "Your bulk ingest has passed verification, and we have requested to start applying the operation. It may required one final approval from an administrator before the operation proceeds.")
+        else
+          @operation.notify(subject: "Bulk Operation - Error creating Github pull request", message: "Your bulk ingest has passed verification, but we had a problem creating a pull request on Github in order to merge this operation with the master branch. Please check your github configuration.")
+        end
+      end
+  end
+end
