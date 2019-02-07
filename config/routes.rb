f75d154e96ea006157732d63aa05e9aaffc0fc43
NedHenry <ethenry@ucsc.edu>
2019-02-07 11:51:59 -0800

    adjust scope and route declarations for engine gem, and add destroy_multiple route

--- config/routes.rb
+++ config/routes.rb
@@ -4,27 +4,22 @@ BulkOps::Engine.routes.draw do
   post 'bulk_ops/github_logout/:user_id', to: 'github_authorization#logout'
   post 'bulk_ops/apply', to: 'operations#apply'
 
-#  scope as: :bulk_ops, module:  :bulk_ops do
-    resources :operations, path: :bulk_ops do
-
-      collection do
-        post :apply
-        get :apply
-        get :search
-        post :search
-      end
-
-      member do
-        post :request_apply
-        post :approve
-        post :edit
-        get :csv
-        get :info
-        get :errors
-        get :log
-        post :duplicate
-      end
+  resources :operations, path: :bulk_ops do
+    
+    collection do
+      get :search
+      post :apply
+      post :search
+      post :destroy_multiple
     end
-#  end
 
+    member do
+      get :csv
+      post :request_apply
+      post :approve
+      post :edit
+      post :duplicate
+    end
+
+  end
 end
