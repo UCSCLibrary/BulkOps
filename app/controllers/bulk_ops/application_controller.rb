0e44413b8bacc72f10a2667e3e398b5463233252
NedHenry <ethenry@ucsc.edu>
2019-02-13 12:44:31 -0800

    add hyrax dependencies directly to application controller

--- app/controllers/bulk_ops/application_controller.rb
+++ app/controllers/bulk_ops/application_controller.rb
@@ -1,5 +1,13 @@
 module BulkOps
   class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
+    include Blacklight::Controller
+    include Hydra::Controller::ControllerBehavior
+
+    # Adds Hyrax behaviors into the application controller 
+    include Hyrax::Controller
+
+    include Hyrax::ThemedLayoutController
+    with_themed_layout '1_column'
   end
 end
