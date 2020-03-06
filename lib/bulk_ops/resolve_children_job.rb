5b80165dcc5ae5cf1a16206f4ae05e9af467194d
NedHenry <ethenry@ucsc.edu>
2020-03-06 15:52:13 -0800

    save parent work before and after setting children to fully process and index the change

--- lib/bulk_ops/resolve_children_job.rb
+++ lib/bulk_ops/resolve_children_job.rb
@@ -2,9 +2,10 @@ class BulkOps::ResolveChildrenJob < ActiveJob::Base
 
   def perform(proxy_id)
     proxy = BulkOps::WorkProxy.find(proxy_id)
-    if proxy.ordered_children.all?{|child| child.work_id.present?}
+    if proxy.work_id.present? && proxy.ordered_children.all?{|child| child.work_id.present?}
       work = ActiveFedora::Base.find(proxy.work_id)
-      work.ordered_member_ids = proxy.ordered_children.map(&:work_id)
+      work.save
+      work.ordered_members = proxy.ordered_children.map{|proxy| ActiveFedora::Base.find(proxy.work_id)}
       work.save
     else
       BulkOps::ResolveChildrenJob.set(wait: 30.minutes).perform_later(proxy_id)
