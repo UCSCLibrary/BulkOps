1c72dbd1293987e6f80c10e7d83d6f6e7295d3d7
Ned Henry <ethenry@ucsc.edu>
2020-03-06 18:15:23 -0800

    refactor interpretation of work type into new file

--- lib/concerns/interpret_options_behavior.rb
+++ lib/concerns/interpret_options_behavior.rb
@@ -13,9 +13,7 @@ module BulkOps::InterpretOptionsBehavior
         @proxy.update(visibility: format_visibility(value))
 
       end
-      if ["worktype","model","type"].include?(normfield)
-        @proxy.update(work_type: format_worktype(value) )
-      end
+
       if ["referenceidentifier", 
           "referenceid", 
           "refid",
@@ -35,15 +33,6 @@ module BulkOps::InterpretOptionsBehavior
 
 
   private 
-
-  def format_worktype(value)
-    # format the value like a class name
-    type = value.titleize.gsub(/[-_\s]/,'')
-    # reject it if it isn't a defined class
-    type = false unless Object.const_defined? type
-    # fall back to the work type defined by the operation, or a standard "Work"
-    return type ||= work_type || operation.work_type || "Work"
-  end
   
   def format_visibility(value)
     case value.downcase
