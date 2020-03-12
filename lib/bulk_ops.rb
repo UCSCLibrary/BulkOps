a880f54ced92646557d85562ac658d1ce737a2fe
NedHenry <ethenry@ucsc.edu>
2020-03-12 11:12:50 -0700

    do not attempt to double-load concerns

--- lib/bulk_ops.rb
+++ lib/bulk_ops.rb
@@ -36,9 +36,11 @@ module BulkOps
   OPTIONS_FILENAME = 'configuration.yml'
   ROW_OFFSET = 2
 
-  dirstring = File.join( File.dirname(__FILE__), 'concerns/*.rb')
-  dirstring2 = File.join( File.dirname(__FILE__), 'bulk_ops/**/*.rb')
-  ((Dir[dirstring] || []) + Dir[dirstring2]).uniq.each  do |file| 
+#  dirstring = File.join( File.dirname(__FILE__), 'concerns/*.rb')
+#  dirstring2 = File.join( File.dirname(__FILE__), 'bulk_ops/**/*.rb')
+#  ((Dir[dirstring] || []) + Dir[dirstring2]).uniq.each  do |file| 
+  dirstring = File.join( File.dirname(__FILE__), 'bulk_ops/**/*.rb')
+  (Dir[dirstring] || []).each  do |file| 
     begin
       require file 
     rescue Exception => e
