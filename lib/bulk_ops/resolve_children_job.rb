505e59bd5735ab3c45178c18008b27fa4e008dbd
Edward Henry <ethenry@ucsc.edu>
2020-12-15 10:40:27 -0800

    include filesets in ordered members

--- lib/bulk_ops/resolve_children_job.rb
+++ lib/bulk_ops/resolve_children_job.rb
@@ -5,7 +5,8 @@ class BulkOps::ResolveChildrenJob < ActiveJob::Base
     if proxy.work_id.present? && proxy.ordered_children.all?{|child| child.work_id.present?}
       work = ActiveFedora::Base.find(proxy.work_id)
       work.save
-      work.ordered_members = proxy.ordered_children.map{|proxy| ActiveFedora::Base.find(proxy.work_id)}
+      ordered_file_sets = work.ordered_members & work.file_sets
+      work.ordered_members = proxy.ordered_children.map{|proxy| ActiveFedora::Base.find(proxy.work_id)} + ordered_file_sets
       work.save
     else
       BulkOps::ResolveChildrenJob.set(wait: 30.minutes).perform_later(proxy_id)
