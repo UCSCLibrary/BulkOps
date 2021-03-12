7ae0e7c099e8ed36cc7fdd63190c6ebb9dad8cff
NedHenry <ethenry@ucsc.edu>
2021-03-11 17:41:43 -0800

    fix error resolving children when filesets and children are present

--- lib/bulk_ops/resolve_children_job.rb
+++ lib/bulk_ops/resolve_children_job.rb
@@ -5,7 +5,7 @@ class BulkOps::ResolveChildrenJob < ActiveJob::Base
     if proxy.work_id.present? && proxy.ordered_children.all?{|child| child.work_id.present?}
       work = ActiveFedora::Base.find(proxy.work_id)
       work.save
-      ordered_file_sets = work.ordered_members & work.file_sets
+      ordered_file_sets = (work.ordered_member_ids & work.file_set_ids).map{|id| ActiveFedora::Base.find(id)}
       work.ordered_members = proxy.ordered_children.map{|proxy| ActiveFedora::Base.find(proxy.work_id)} + ordered_file_sets
       work.save
       proxy.ordered_children.each{|proxy| ActiveFedora::Base.find(proxy.work_id).save}
